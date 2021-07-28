12de8d00d4718f2a4a284fce14f5aaba0e46498d
Martin Pszczola <martinpsz@protonmail.com>
2021-07-28 13:30:24 -0700

    Initial Commit with SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,31 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'UPTE UCSD Local 9',
+    description: 'UPTE Local 9, a local of UPTE-CWA Local 9119, represents Healthcare, Research and Technical workers at the University of California, San Diego.',
+    menuLinks: [
+      {
+        name: 'About',
+        link: '#about'
+      },
+      {
+        name: 'Upcoming Events',
+        link: '#events'
+      },
+      {
+        name: 'Become a Member',
+        link: '#membership'
+      },
+      {
+        name: 'Contact Us',
+        link: '#contact'
+      },
+      {
+        name: 'Register/Sign In',
+        link: '/members'
+      }
+    ]
+  },
+  plugins: [`gatsby-plugin-react-helmet`],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import '../style.css'
+import Seo from "../components/seo"
+import Banner from "../components/Banner/banner"
+import Navbar from "../components/Navbar/navbar"
+import Header from "../components/Header/header"
+import About from "../components/About/about"
+import Upcoming from "../components/Upcoming/upcoming"
+import Join from "../components/Join/join"
+import Footer from "../components/Footer/footer"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Seo/>
+      <Banner/>
+      <Navbar/>
+      <Header/>
+      <About />
+      <Upcoming />
+      <Join />
+      <Footer/>
+    </>
+  )
 }
